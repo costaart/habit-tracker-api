0b0e7cfc68fdb5d7371187fe0726bfbbb0c25c44
Arthur Almeida <artcosta1@gmail.com>
2025-05-28 21:34:59 -0300

    feat: add weekly report command, notification, and email template

---
+++ app/Console/Commands/WeeklyReportCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use App\Notifications\WeeklyReport;
+use Illuminate\Console\Command;
+
+class WeeklyReportCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'report:weekly';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send weekly report';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $user = User::first();
+
+        $user->notify(
+            new WeeklyReport()
+        );
+    }
+}
